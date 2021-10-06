44327dd08ca3d3b67dcf2bf83574c1e16fa751d6
lynam <lynamadjda@gmail.com>
2021-10-06 12:56:16 +0200

    preload

--- project/index.js
+++ project/index.js
@@ -1,13 +1,35 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
+  // Créer la fenêtre de navigation.
+  try{
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+      preload: path.join(__dirname, './preload.js')
+      }
     })
     win.loadFile('./views/index.html')
   }
+  catch(e){
+    console.log(e)
+  }
+}
 
-  app.whenReady().then(() => {
-    createWindow()
+// Cette méthode sera appelée quand Electron aura fini de s'initialiser
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    // Sur macOS il est d'usage de recréer une fenêtre dans l'application quand
+    // l'icône du dock est cliquée et qu'il n'y a pas d'autre fenêtre ouverte.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
+})
+
+//Quitter quand toutes les fenêtres sont fermées, sauf sur macOS.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+})
+
---
+++ project/preload.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      console.log(dependency+"-version : " + process.versions[dependency]);
+    }
+  })
