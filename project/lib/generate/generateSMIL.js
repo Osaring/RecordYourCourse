060b5948e639bccb18fba392b5969c352d3cf3c6
Osaring <echaubard.valentin@gmail.com>
2021-10-21 12:42:56 +0200

    First timer

--- project/lib/generate/generateSMIL.js
+++ project/lib/generate/generateSMIL.js
@@ -2,11 +2,26 @@ const fs = require('fs');
 const { dirname } = require('path');
 const path = require('path');
 const chemin = __dirname + '/../../history/cours1/';
+const clock = document.getElementById('timer');
+var hrs = 0, min = 0, sec = 0;
+var start = false;
 
 const generateSMIL = document.querySelector('button#generate');
 generateSMIL.addEventListener('click', async () => {
     copyTemplateSmil();
-
+    if(!start){
+        start = true;
+        timer();
+    }
+    /*start.onclick = timer;
+    stop.onclick = function() {
+        clearTimeout(t);
+    }
+    reset.onclick = function() {
+        h1.textContent = "00:00:00";
+        seconds = 0; minutes = 0; hours = 0;
+    }
+    */
 });
 
 function copyTemplateSmil(){
@@ -16,4 +31,35 @@ function copyTemplateSmil(){
             throw err;
         }
     });
+}
+
+function tick(){
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
+
+function add() {
+    tick();
+    console.log(hrs+" "+min+" "+sec);
+    clock.innerHTML = (hrs > 9 ? hrs : "0" + hrs) 
+        	 + ":" + (min > 9 ? min : "0" + min)
+       		 + ":" + (sec > 9 ? sec : "0" + sec);     
+    timer();
+}
+
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+function addDiapo(){
+}
+
+function beginTimer(){
 }
