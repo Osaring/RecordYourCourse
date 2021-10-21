8ae6a89583e1cca34b2a3d0921fbdcc7fc9f872e
NicolasLeFaou <lefa.nicolas@gmail.com>
2021-10-21 10:49:01 +0200

    fix promise import file

--- project/lib/importFile/importPPT.js
+++ project/lib/importFile/importPPT.js
@@ -1,50 +1,56 @@
 const JSZip = require('jszip')
 const FileSaver = require('file-saver');
+const zip = new JSZip();
 
 async function importPresentation() {
-    return new Promise((resolve,reject)=>{
+    document.getElementById('file').click();
+    const p = new Promise (function (resolve, reject) {
+        document.getElementById('file').addEventListener('change', (e) => {
 
-        document.getElementById('file').click();
-        document.getElementById("file").addEventListener('change', (e) => {
-            while (e.target.files && e.target.files.length === 0){
+            if (e.target.files) {
                 const file = e.target.files[0];
-                const extension = file.name.split('.');
+                console.log("length ", e.target.files.length)
+                const splitFilename = file.name.split('.');
                 console.log("metadata file ", file)
-                console.log("filename ", file.name)
-                console.log("extension file ", extension[extension.length-1]);
-                zipFile(file); // zip le fichier importé
-                console.log("fileeee return")
+                console.log("extension file ", splitFilename[splitFilename.length - 1]);
+
+                zipFile().catch((err) => {
+                    console.log(err);
+                });
+
+                // unzipFile(splitFilename[Ø]+".zip");
+                console.log("fin importFile")
+                resolve("ok")
             }
         })
     })
+    return p;
 }
 
 function zipFile(file) {
-    return new Promise((resolve,reject)=>{
+    const p = new Promise (function (resolve, reject) {
+        console.log("début zipFile");
 
-        const zip = new JSZip();
         const name = file.name.split('.')[0] + ".zip";
         zip.file(name, file);
         zip.generateAsync({type:"blob"})
         .then(function(content) {
             saveAs(content, name);
-            /*
-            var item = {
-            'name': name,
-            'type': content.type,
-            'size': content.size,
-            'guid': generatorguid()
-            };
-            */
         });
-        console.log("zipfile func")
+        console.log("fin zipFile")
+        resolve("ok")
+    })
+    return p;
+}
+
+function unzipFile(file) {
+    zip.loadAsync(file).then(function (zip) {
+        Object.keys(zip.files).forEach(function (filename) {
+            zip.files[filename].async('string').then(function (fileData) {
+                console.log(fileData) // These are your file contents
+            })
+        })
     })
 }
 
-function getXMLFile(zipFile) {
-    console.log("zipFile ", zipFile)
-    return ;
-}
-
-
-export { importPresentation, zipFile, getXMLFile };
+export { importPresentation, zipFile, unzipFile };
